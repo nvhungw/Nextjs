6ada0b1387e23613b22b47643675ef81366cffce
nvhungw <nvhung2212.w@gmail.com>
2023-08-15 23:27:43 +0700

    update

---
+++ src/views/layouts/Navbar.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const Navbar = () => {
+    return(
+        <ul>
+            <li><Link href="/home">Home</Link></li>
+            <li><Link href="/blog">Blog</Link></li>
+            <li><Link href="/about">About</Link></li>
+        </ul>
+    )
+}
+
+export default Navbar
